97e0a8f0922b7aeaa80a48b5bae63079db18e5dd
Ethan Uphoff <megameowthethird@gmail.com>
2021-09-30 20:44:56 -0500

    First Commit

---
+++ scripts/init.lua
@@ -0,0 +1,5 @@
+ScriptHost:LoadScript("scripts/class.lua")
+ScriptHost:LoadScript("scripts/custom_item.lua")
+ScriptHost:LoadScript("scripts/settings.lua")
+Tracker:AddItems("items/items.json")
+Tracker:AddLayouts("layouts/tracker.json")
